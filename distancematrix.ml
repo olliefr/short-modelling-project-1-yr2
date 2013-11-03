1f020ea6dffa1c5a2adbf1b6a33f597532f42a0d
Ollie Frolovs <o.frolovs@gmail.com>
2013-11-03 22:54:50 +0000

    introduced command line options; refactoring; this commit is a regress though, need further work

--- distancematrix.ml
+++ distancematrix.ml
@@ -10,48 +10,40 @@
 open Core.Std
 open Lwt
 
-(* Google API wants parameters separated by pipe rather than a comma.
-   This is a violation of the RFC. 
-   Uri does not provide for this as of 1.3.10
-   But Google API seems to be happy with character escaped version. *)
-let anti_rfc_fy s = String.concat ~sep:"|" s
-
-let base_uri_string = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-
-(* TODO these be read from a file or standard input *)
-(* TODO read from standard input *)
-(* TODO if --input file is given, read from file *)
-
-
+(** Input/Output related functions *)
 (* prune any whitespace-only or empty lines from the input *)
 let prune list = 
   list
   |> List.map ~f:String.strip
   |> List.filter ~f:(fun x -> not (String.is_empty x))
 
-(* FIXME error be here if file not found *)
-(* TODO use with_file instead *)
-let read_input_f name =
-  let file = In_channel.create name in
-  let nodes = In_channel.input_lines file in
-  In_channel.close file;
-  nodes
-
-(*
-name
-|> In_channel.create
-|> In_channel.input_lines
-*)
-
-(* FIXME this is a hack; should be done externally *)
-let universify s = String.concat ~sep:" " [s; "University"]
-
-(* FIXME should this not be merged with read_input_f somehow? *)
 (* we consider any non-blank line a valid input and prune any other input *)
-let read_input_stdin = 
-  In_channel.input_lines stdin
-  |> prune
-  |> List.map ~f:universify
+let read_input ic = prune (In_channel.input_lines ic)
+
+let choose_input_channel ifile = 
+  match ifile with
+      "-"      -> In_channel.stdin
+    | filename -> In_channel.create filename
+
+let choose_output_channel ofile = 
+  match ofile with
+      "-" -> Out_channel.stdout
+    | _   -> Out_channel.stdout
+
+(** End of input/output related functions *)
+
+
+
+
+(** Uri construction related functions *)
+(* Google API wants parameters separated by pipe rather than a comma.
+   This is a violation of the RFC. 
+   Uri does not provide for this as of 1.3.10
+   But Google API seems to be happy with character escaped version. *)
+(* FIXME rfc does not actually disallow pipes as long as they are % encoded *)
+let anti_rfc_fy s = String.concat ~sep:"|" s
+
+let base_uri_string = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
 (* creates the uri to query distances from town t to list of nodes ts *)
 let create_uri_for t ts = 
@@ -60,6 +52,12 @@ let create_uri_for t ts =
 				 ("destinations", [anti_rfc_fy ts]);
 				 ("sensor", ["false"])]
 
+(** End of uri construction related functions *)
+
+
+
+
+(** HTTP client functions *)
 (* FIXME this is where the connection might fail; upd: dafuq, why exception when it returns option? *)
 let run_matrix_query uri =
   let response = Lwt_main.run (Cohttp_lwt_unix.Client.get uri) in
@@ -67,10 +65,21 @@ let run_matrix_query uri =
       None -> failwith "error: request failed -- any reason"
     | Some (_,b) -> Lwt_main.run (Cohttp_lwt_body.string_of_body b)
 
+(** End of HTTP client functions *)
+
+
+
+
+
+(** JSON parsing related functions *)
 (* TODO should JSON related code not be in a separate file *)
 
 let pretty_print_json json = Yojson.Basic.pretty_to_channel Out_channel.stdout json
 
+(* FIXME more complex type needed? *)
+(*
+type response = Failure of string | element
+*)
 type element = {
   status : string;
   distance : int;
@@ -79,11 +88,13 @@ type element = {
 }
 
 (* FIXME how to avoid this ugly hack with name *)
-let json_element e = Yojson.Basic.Util.({
-  status = e |> member "status" |> to_string;
-  distance = e |> member "distance" |> member "value" |> to_int;
-  duration = e |> member "duration" |> member "value" |> to_int;
-  name = ""
+(* FIXME why pattern matching does not work inside records? *)
+let json_element e = Yojson.Basic.Util.(
+  let status = e |> member "status" |> to_string in {
+    status;
+    distance = if status = "OK" then e |> member "distance" |> member "value" |> to_int else 0;
+    duration = if status = "OK" then e |> member "duration" |> member "value" |> to_int else 0;
+    name = ""
 })
 
 let bootstrap_json_from body = Yojson.Basic.from_string body
@@ -101,56 +112,105 @@ let origin_from_json json =  Yojson.Basic.Util.(
 let process_response json = 
   let open Yojson.Basic.Util in
   let status = json |> member "status" |> to_string in
-(*  let origin_addresses = json |> member "origin_addresses" |> to_list |> filter_string in *)
-  let destination_addresses = json |> member "destination_addresses" |> to_list |> filter_string in
-  let row = List.hd (json |> member "rows" |> to_list) in
-  let elements = match row with 
-      None -> failwith "empty row"
-    | Some (row) -> row |> member "elements" |> to_list in 
-  let data = List.map2_exn destination_addresses elements 
-    ~f:(fun destination json -> { (json_element json) with name = destination }) in
-  (status, data)
+  match status with
+      "OK" ->
+	(*  let origin_addresses = json |> member "origin_addresses" |> to_list |> filter_string in *)
+	let destination_addresses = json |> member "destination_addresses" |> to_list |> filter_string in
+	let row = List.hd (json |> member "rows" |> to_list) in
+	let elements = match row with 
+	    None -> failwith "empty row"
+	  | Some (row) -> row |> member "elements" |> to_list in 
+	let data = List.map2_exn destination_addresses elements 
+	  ~f:(fun destination json -> { (json_element json) with name = destination }) in
+	(status, Some data)
+    | _ -> (status, None)
+
+(** End of JSON functions*)
+
+
+
 
 type outcome = {
   destination : string;
   result : element
 }
 
+(* TODO should these be optional command line parameters? *)
 (* FIXME stupid limitation of 100 elements per 10 sec; yet good enough for our project *)
 let limit = 100  (* elements = origins * destinations *)
 let timeout = 10 (* seconds *)
 
-let outcome = 
-  let nodes = read_input_stdin in
-  let _ = begin
-    eprintf "-- read input, N = %i\n" (List.length nodes);
-    if List.length nodes >= 100 then failwith "can only linearize less than 100 elements";
-    eprintf "-- downloading data for:\n%!"
-  end in 
-  let rec get_distances t (result, nodes, processed) = 
-    if processed + (List.length nodes) >= limit
-    then begin Unix.sleep timeout; get_distances t (result, nodes, 0) end 
-    else
-      let _ = eprintf "   -- %s... %!" t in
-      let uri = (create_uri_for t nodes) in
-      let response = run_matrix_query uri in
-      let json = bootstrap_json_from response in
-      let (status, r) = process_response json in
-      let _ = eprintf "%s\n%!" status in
-      ((origin_from_json json, r)::result, nodes, processed)
-  in match List.fold_right nodes ~f:get_distances ~init:([], nodes, 0) with (o,_,_) -> o
+(*
+let download_all_distances ifile ofile () = 
+  let outcome = 
+    let nodes = read_input_f ifile in
+    let _ = begin
+      eprintf "-- read input, N = %i\n" (List.length nodes);
+      if List.length nodes >= 100 then failwith "can only linearize less than 100 elements";
+      eprintf "-- downloading data for:\n%!"
+    end in 
 
-let _ = eprintf "-- download complete\n%!"
 
-(* TODO output to the standard output *)
-let _ = List.iter outcome ~f:(fun (origin,destinations) -> 
-  List.iter destinations 
-    ~f:(fun destination -> if origin <> destination.name then printf "%s|%s|%s|%i|%i\n%!" 
-      destination.status 
-      origin 
-      destination.name
-      destination.distance
-      destination.duration))
+    let rec get_distances t (result, nodes, processed) = 
+      if processed + (List.length nodes) >= limit
+      then 
+	begin 
+	  eprintf "-- sleeping for %i seconds\n%!" timeout;
+	  Unix.sleep timeout; 
+	  get_distances t (result, nodes, 0) 
+	end 
+      else
+	let _ = eprintf "   -- %s... %!" t in
+	let uri = (create_uri_for t nodes) in
+	let response = run_matrix_query uri in
+	let json = bootstrap_json_from response in
+	let (status, r) = process_response json in
+	let _ = eprintf "%s\n%!" status in
+	((origin_from_json json, r)::result, nodes, processed + List.length nodes)
+    in match List.fold_right nodes ~f:get_distances ~init:([], nodes, 0) with (o,_,_) -> o
+  in let _ = eprintf "-- download complete\n%!" in
 
-(* TODO if --output file is given, write to the file *)
-(* TODO but what is the desired format for the output? *)
+     let _ = List.iter outcome ~f:(fun (origin,destinations) -> match destinations with
+	 None -> printf "ERROR \n%!"
+       | Some destinations ->
+	 List.iter destinations 
+	   ~f:(fun destination -> if origin <> destination.name then printf "%s|%s|%s|%i|%i\n%!" 
+	       destination.status 
+	       origin 
+	       destination.name
+	       destination.distance
+	       destination.duration)) in ()
+*)
+ 
+(* TODO what is the desired format for the output? *)
+
+let download_all_distances ifile ofile () =
+  let ic = choose_input_channel  ifile in
+  let oc = choose_output_channel ofile in
+  let locations = read_input ic in
+  let number_of_locations = List.length locations in
+  let () = if number_of_locations >= limit then failwith 
+      (sprintf "can only download the data for 100 or fewer locations, you have %i" number_of_locations) in
+  let () = eprintf "-- read %i node%s\n%!" number_of_locations (if number_of_locations = 1 then "" else "s") in
+(*  List.iter locations ~f:(fun x -> eprintf "-- Location: %s\n%!" x) *)
+  let distance_matrix = [
+    { status = "OK"; name = "University of Gruffalo"; distance = 1;   duration = 2 };
+    { status = "OK"; name = "University of Moust";    distance = 100; duration = 120 };
+    { status = "OK"; name = "University of Boofle";   distance = 200; duration = 220 }
+  ] in
+  List.iter distance_matrix ~f:(fun x -> fprintf oc "%s|%s|%i|%i\n" x.status x.name x.distance x.duration);
+  Out_channel.flush oc
+  (* FIXME how can i close oc if i don't know it it's stdout? *)
+
+let command = 
+  Command.basic
+    ~summary:"Download distance/duration for trips between given locations from Google Distance Matrix"
+    Command.Spec.(
+      empty 
+      +> flag "-input"  (optional_with_default "-" file)  ~doc:"file input file"
+      +> flag "-output" (optional_with_default "-" file)  ~doc:"file output file"
+    )
+    download_all_distances
+
+    
+let () = Command.run ~version:"1.0" ~build_info:"olliefr" command
