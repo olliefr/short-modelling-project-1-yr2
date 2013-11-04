a4d3919c558fe04bb4706196f75cd8f1ab9a552b
Ollie Frolovs <o.frolovs@gmail.com>
2013-11-04 13:23:48 +0000

    finally, all the refactoring paid off

--- distancematrix.ml
+++ distancematrix.ml
@@ -10,6 +10,13 @@
 open Core.Std
 open Lwt
 
+type route = {
+  origin : string;
+  destination : string;
+  distance : int;
+  duration : int
+}
+
 (** Input/Output related functions *)
 (* prune any whitespace-only or empty lines from the input *)
 let prune list = 
@@ -30,26 +37,32 @@ let choose_output_channel ofile =
       "-" -> Out_channel.stdout
     | _   -> Out_channel.stdout
 
+(* TODO what is the desired format for the output? *)
+(* FIXME how can i close oc if i don't know it it's stdout? *)
+let print_distance_matrix distance_matrix oc = 
+  List.iter distance_matrix ~f:(fun y ->
+    List.iter y ~f:(fun x -> match x with 
+	Error err -> fprintf oc "error %s\n" err
+      | Ok x -> fprintf oc "%s|%s|%i|%i\n" x.origin x.destination x.distance x.duration)
+  );
+  Out_channel.flush oc
+
 (** End of input/output related functions *)
-
-
 
 
 (** Uri construction related functions *)
 (* Google API wants parameters separated by pipe rather than a comma.
-   This is a violation of the RFC. 
    Uri does not provide for this as of 1.3.10
-   But Google API seems to be happy with character escaped version. *)
-(* FIXME rfc does not actually disallow pipes as long as they are % encoded *)
-let anti_rfc_fy s = String.concat ~sep:"|" s
+   But Google API seems to be happy with url-encoded version. *)
+let concat_with_pipe s = String.concat ~sep:"|" s
 
 let base_uri_string = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
 (* creates the uri to query distances from town t to list of nodes ts *)
-let create_uri_for t ts = 
+let create_uri_for t locations = 
   let base_uri = Uri.of_string base_uri_string in
   Uri.add_query_params base_uri [("origins", [t]);
-				 ("destinations", [anti_rfc_fy ts]);
+				 ("destinations", [concat_with_pipe locations]);
 				 ("sensor", ["false"])]
 
 (** End of uri construction related functions *)
@@ -68,121 +81,66 @@ let run_matrix_query uri =
 (** End of HTTP client functions *)
 
 
-
-
-
 (** JSON parsing related functions *)
 (* TODO should JSON related code not be in a separate file *)
 
 let pretty_print_json json = Yojson.Basic.pretty_to_channel Out_channel.stdout json
 
-(* FIXME more complex type needed? *)
-(*
-type response = Failure of string | element
-*)
-type element = {
-  status : string;
-  distance : int;
-  duration : int;
-  name : string
-}
-
-(* FIXME how to avoid this ugly hack with name *)
-(* FIXME why pattern matching does not work inside records? *)
-let json_element e = Yojson.Basic.Util.(
-  let status = e |> member "status" |> to_string in {
-    status;
-    distance = if status = "OK" then e |> member "distance" |> member "value" |> to_int else 0;
-    duration = if status = "OK" then e |> member "duration" |> member "value" |> to_int else 0;
-    name = ""
-})
-
-let bootstrap_json_from body = Yojson.Basic.from_string body
-
-let origin_from_json json =  Yojson.Basic.Util.(
-  match List.hd (json |> member "origin_addresses" |> to_list |> filter_string) with
-      None -> "???"
-    | Some x -> x
-)
-
-(* TODO json toplevel status must be "OK", otherwise give a warning *)
-(* TODO json element status must be "OK", otherwise give a warning *)
 (* FIXME this p-arser expects the matrix to be a row vector. works for me, but not clever *)
-
-let process_response json = 
+(* FIXME if the server returns empty string, this will fail; what to do? *)
+(* Assumption: the locations have been validated beforehand; we use original names, not those returned by the Matirx *)
+let locations_from_json s origin destinations = 
+  let json = Yojson.Basic.from_string s in
   let open Yojson.Basic.Util in
-  let status = json |> member "status" |> to_string in
-  match status with
+  match json |> member "status" |> to_string with
       "OK" ->
-	(*  let origin_addresses = json |> member "origin_addresses" |> to_list |> filter_string in *)
-	let destination_addresses = json |> member "destination_addresses" |> to_list |> filter_string in
-	let row = List.hd (json |> member "rows" |> to_list) in
-	let elements = match row with 
-	    None -> failwith "empty row"
-	  | Some (row) -> row |> member "elements" |> to_list in 
-	let data = List.map2_exn destination_addresses elements 
-	  ~f:(fun destination json -> { (json_element json) with name = destination }) in
-	(status, Some data)
-    | _ -> (status, None)
-
+	let elements = 
+	  [json] 
+            |> filter_member "rows"
+            |> flatten
+            |> filter_member "elements"
+            |> flatten
+	in Ok (List.map2_exn destinations elements 
+		 ~f:(fun destination element -> 
+		   match element |> member "status" |> to_string with
+		       "OK" -> Ok {
+			 origin;
+			 destination;
+			 distance =  element |> member "distance" |> member "value" |> to_int;
+			 duration =  element |> member "duration" |> member "value" |> to_int
+		       }
+		     | err -> Error err))
+    | err -> Error err
+	
 (** End of JSON functions*)
 
 
-
-
-type outcome = {
-  destination : string;
-  result : element
-}
 
 (* TODO should these be optional command line parameters? *)
 (* FIXME stupid limitation of 100 elements per 10 sec; yet good enough for our project *)
 let limit = 100  (* elements = origins * destinations *)
 let timeout = 10 (* seconds *)
 
-(*
-let download_all_distances ifile ofile () = 
-  let outcome = 
-    let nodes = read_input_f ifile in
-    let _ = begin
-      eprintf "-- read input, N = %i\n" (List.length nodes);
-      if List.length nodes >= 100 then failwith "can only linearize less than 100 elements";
-      eprintf "-- downloading data for:\n%!"
-    end in 
+(* runs a query for one location *)
+let query_location location locations =
+  let uri = create_uri_for location locations in
+  let json = run_matrix_query uri in
+  locations_from_json json location locations
 
-
-    let rec get_distances t (result, nodes, processed) = 
-      if processed + (List.length nodes) >= limit
-      then 
-	begin 
-	  eprintf "-- sleeping for %i seconds\n%!" timeout;
-	  Unix.sleep timeout; 
-	  get_distances t (result, nodes, 0) 
-	end 
-      else
-	let _ = eprintf "   -- %s... %!" t in
-	let uri = (create_uri_for t nodes) in
-	let response = run_matrix_query uri in
-	let json = bootstrap_json_from response in
-	let (status, r) = process_response json in
-	let _ = eprintf "%s\n%!" status in
-	((origin_from_json json, r)::result, nodes, processed + List.length nodes)
-    in match List.fold_right nodes ~f:get_distances ~init:([], nodes, 0) with (o,_,_) -> o
-  in let _ = eprintf "-- download complete\n%!" in
-
-     let _ = List.iter outcome ~f:(fun (origin,destinations) -> match destinations with
-	 None -> printf "ERROR \n%!"
-       | Some destinations ->
-	 List.iter destinations 
-	   ~f:(fun destination -> if origin <> destination.name then printf "%s|%s|%s|%i|%i\n%!" 
-	       destination.status 
-	       origin 
-	       destination.name
-	       destination.distance
-	       destination.duration)) in ()
-*)
- 
-(* TODO what is the desired format for the output? *)
+(* TODO put failwith/error in a separate function, we only care about OVER_QUERY_LIMIT here *)
+let rec query_location_with_retry location locations = 
+  match query_location location locations with
+      Ok distance_vector -> distance_vector
+    | Error "INVALID_REQUEST" -> failwith "invalid request"
+    | Error "MAX_ELEMENTS_EXCEEDED" -> failwith "the product of origins and destinations exceeds the per-query limit"
+    | Error "OVER_QUERY_LIMIT" -> begin
+      eprintf "-- query limit exceeded; sleeping for %is\n%!" timeout;
+      Unix.sleep timeout;
+      query_location_with_retry location locations
+    end
+    | Error "REQUEST_DENIED" -> failwith "the service denied use of the Distance Matrix service"
+    | Error "UNKNOWN_ERROR" -> failwith "server error"
+    | Error x -> failwith x
 
 let download_all_distances ifile ofile () =
   let ic = choose_input_channel  ifile in
@@ -192,16 +150,10 @@ let download_all_distances ifile ofile () =
   let () = if number_of_locations >= limit then failwith 
       (sprintf "can only download the data for 100 or fewer locations, you have %i" number_of_locations) in
   let () = eprintf "-- read %i node%s\n%!" number_of_locations (if number_of_locations = 1 then "" else "s") in
-(*  List.iter locations ~f:(fun x -> eprintf "-- Location: %s\n%!" x) *)
-  let distance_matrix = [
-    { status = "OK"; name = "University of Gruffalo"; distance = 1;   duration = 2 };
-    { status = "OK"; name = "University of Moust";    distance = 100; duration = 120 };
-    { status = "OK"; name = "University of Boofle";   distance = 200; duration = 220 }
-  ] in
-  List.iter distance_matrix ~f:(fun x -> fprintf oc "%s|%s|%i|%i\n" x.status x.name x.distance x.duration);
-  Out_channel.flush oc
-  (* FIXME how can i close oc if i don't know it it's stdout? *)
-
+  (*  List.iter locations ~f:(fun x -> eprintf "-- Location: %s\n%!" x) *)
+  let distance_matrix = List.map locations ~f:(fun location -> query_location_with_retry location locations) in
+  print_distance_matrix distance_matrix oc
+    
 let command = 
   Command.basic
     ~summary:"Download distance/duration for trips between given locations from Google Distance Matrix"
